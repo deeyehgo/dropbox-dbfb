75cb1d29c6466089dc15aef095c86413b7195f1c
Diego <diego.delgado.a@gmail.com>
2014-10-03 11:08:15 -0700

    Add offset scroll effect for hero

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,20 @@
+// TODO: retina all images
+
 'use strict';
 (function () {
-  var carousel = new DBCarousel(document.querySelector('.carousel'));
-  console.log(carousel);  
+  var carousel,
+    hero,
+    offsetSpeed;
+
+  offsetSpeed = 0.7;
+
+  carousel = new DBCarousel(document.querySelector('.carousel'));
+  hero = document.querySelector('.hero');
+  
+  function handleScroll() {
+    hero.style.backgroundPosition = '0 ' + -(hero.getBoundingClientRect().top * offsetSpeed) + 'px';
+
+  }
+  window.addEventListener('scroll', handleScroll);
+  // retina
 }());
